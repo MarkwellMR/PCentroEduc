75bdcbe36aa46ba873c8c4475e89d10cf922f625
Usuario <Usuario@10.0.2.15>
2018-08-30 08:56:45 -0600

    se agrego busqueda por nombre en encargado y notas

---
+++ src/java/com/centroeduc/dao/NotasDAO.java
@@ -0,0 +1,9 @@
+package com.centroeduc.dao;
+
+import com.centroeduc.model.Conexion;
+import com.centroeduc.model.Notas;
+
+public class NotasDAO extends Conexion{
+    Notas notas = new Notas();
+    
+}
---
+++ src/java/com/centroeduc/model/Notas.java
@@ -0,0 +1,16 @@
+package com.centroeduc.model;
+
+public class Notas {
+    private int codNota;
+    private int nota;
+    
+    public Notas(){
+        
+    }
+    
+    public Notas(int codNota, int nota){
+    this.codNota= codNota;
+    this.nota=nota;
+    }
+    
+}
